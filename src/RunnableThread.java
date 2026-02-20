2b4be897a02a0473f4a810a726f73457601a8387
dragdab <139127963+dragdab@users.noreply.github.com>
2026-02-20 09:48:45 +0530

    Implement run method in MyRunnable class

--- src/RunnableThread.java
+++ src/RunnableThread.java
@@ -1,12 +1,23 @@
+
 class MyRunnable implements Runnable {
-    // TODO: Override run() method
-    // Loop from 1 to 5 and print numbers separated by space
+    
+    @Override
+    public void run() {
+        // Loop from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " ");
+        }
+    }
 }
 
 public class RunnableThread {
     public static void main(String[] args) {
-        // TODO: Create an instance of MyRunnable
-        // TODO: Create a Thread object passing the runnable instance
-        // TODO: Start the thread
+
+        MyRunnable myRunnable = new MyRunnable();
+
+
+        Thread thread = new Thread(myRunnable);
+
+        thread.start();
     }
 }
